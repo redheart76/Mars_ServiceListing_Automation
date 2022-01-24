5c87819ef1eab24514f8cf2a182a5b84cf04d61d
redheart76 <redheart76@gmail.com>
2022-01-24 13:58:24 +1300

    no message

---
+++ Mars_ServiceListingPage_Automation/Features/EditMySkill.feature
@@ -0,0 +1,12 @@
+﻿Feature: Edit my existing skill
+	In order to edit a skill in the listing
+	As an user
+	I want to edit the skill details
+	
+@mytag
+Scenario: Edit my skill details
+	Given I logged in my account
+	And I have navigated to listing management page
+    And I have navigated to edit skill details
+	When I press save
+	Then the result change will be saved
---
+++ Mars_ServiceListingPage_Automation/Tests/EditMySkillSteps.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using Mars_ServiceListingPage_Automation.Pages;
+using Mars_ServiceListingPage_Automation.Utilities;
+using TechTalk.SpecFlow;
+
+namespace Mars_ServiceListingPage_Automation.Tests
+{
+    [Binding]
+    public class EdiMySkillSteps : CommonDriver
+    {
+        private readonly ScenarioContext _scenarioContext;
+
+        public EdiMySkillSteps(ScenarioContext scenarioContext)
+        {
+            _scenarioContext = scenarioContext;
+        }
+        [Given(@"I logged in my account")]
+        public void GivenILoggedInMyAccount()
+        {
+            HomePage homepageObj = new HomePage();
+            homepageObj.loginSteps(driver);
+        }
+
+        [Given(@"I have navigated to listing management page")]
+        public void GivenIHaveNavigatedToListingManagementPage()
+        {
+            ProfilePage profilePageObj = new ProfilePage();
+            profilePageObj.gotoListingManagementPage(driver);
+        }
+
+        [Given(@"I have navigated to edit skill details")]
+        public void GivenIHaveNavigatedToEditSkillDetails()
+        {
+            ListingManagermentPage listingManagermentPageObj = new ListingManagermentPage();
+            listingManagermentPageObj.editSkill(driver);
+            ServiceListingPage serviceListingPageObj = new ServiceListingPage();
+            serviceListingPageObj.editDescription(driver);
+
+        }
+
+        [When(@"I press save")]
+        public void WhenIPressSave()
+        {
+            ServiceListingPage serviceListingPageObj = new ServiceListingPage();
+            serviceListingPageObj.saveSkill(driver);
+        }
+
+        [Then(@"the result change will be saved")]
+        public void ThenTheResultChangeWillBeSaved()
+        {
+            ListingManagermentPage listingManagermentPageObj = new ListingManagermentPage();
+            listingManagermentPageObj.assertNewDescriptionIsDisplayed(driver);
+            driver.Close();
+        }
+    }
+}
